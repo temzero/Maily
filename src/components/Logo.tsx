8a31239a4e1b9861afba2422367ac2cc8025a23b
temzero <nguyentrannhan.00@gmail.com>
2026-05-25 16:30:34 +0700

    Update Logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,58 +1,62 @@
-import { Component } from 'solid-js';
-import { useNavigate } from '@solidjs/router'; // or 'solid-start' depending on your router
-import { APP_NAME } from '~/data/constants';
-import LogoIcon from '~/assets/logo.svg';
-
+import { Component } from "solid-js";
+import { useNavigate } from "@solidjs/router"; // or 'solid-start' depending on your router
+import { APP_NAME } from "~/data/constants";
+import LogoIcon from "~/assets/logo.svg";
+import logoIconRaw from "~/assets/logo.svg?raw";
 
 interface LogoProps {
-    size?: 'sm' | 'md' | 'lg' | 'xl';
-    class?: string;
-    showName?: boolean;
-    onClick?: (e: MouseEvent) => void;
-    preventDefault?: boolean;
+  size?: "sm" | "md" | "lg" | "xl";
+  class?: string;
+  showName?: boolean;
+  onClick?: (e: MouseEvent) => void;
+  preventDefault?: boolean;
 }
 
 const sizeMap = {
-    sm: { logoIcon: 'w-3 h-3', name: 'text-sm', gap: 'gap-0.5' }, // 12px
-    md: { logoIcon: 'w-5 h-5', name: 'text-base', gap: 'gap-1' }, // 16px
-    lg: { logoIcon: 'w-9 h-9', name: 'text-2xl', gap: 'gap-2' }, // 32px
-    xl: { logoIcon: 'w-12 h-12', name: 'text-4xl', gap: 'gap-2' }, // 48px
+  sm: { logoIcon: "[&_svg]:w-5! [&_svg]:h-5!", name: "text-sm", gap: "gap-0.5" },
+  md: { logoIcon: "[&_svg]:w-7! [&_svg]:h-7!", name: "text-base", gap: "gap-0.5" },
+  lg: { logoIcon: "[&_svg]:w-10! [&_svg]:h-10!", name: "text-2xl", gap: "gap-1" },
+  xl: { logoIcon: "[&_svg]:w-14! [&_svg]:h-14!", name: "text-4xl", gap: "gap-2" },
 };
 
 const Logo: Component<LogoProps> = (props) => {
-    const navigate = useNavigate();
-    const size = () => props.size ?? 'md';
-    const showName = () => props.showName ?? true;
-    const s = () => sizeMap[size()];
+  const navigate = useNavigate();
+  const size = () => props.size ?? "md";
+  const showName = () => props.showName ?? true;
+  const s = () => sizeMap[size()];
 
-    const handleClick = (e: MouseEvent) => {
-        if (props.preventDefault) {
-            e.preventDefault();
-        }
+  const handleClick = (e: MouseEvent) => {
+    if (props.preventDefault) {
+      e.preventDefault();
+    }
 
-        if (props.onClick) {
-            props.onClick(e);
-        } else {
-            navigate('/inbox');
-        }
-    };
+    if (props.onClick) {
+      props.onClick(e);
+    } else {
+      navigate("/inbox");
+    }
+  };
 
-    return (
+  return (
+    <div
+      onClick={handleClick}
+      class={`inline-flex items-center cursor-pointer hover:scale-110 transition-all ${s().gap} ${props.class}`}
+    >
         <div
-            onClick={handleClick}
-            class={`inline-flex items-center cursor-pointer hover:scale-110 transition-all ${s().gap} ${props.class}`}
+          innerHTML={logoIconRaw}
+          class={`flex items-center justify-center ${s().logoIcon} leading-none select-none [&_svg]:block`}
+        />
+      {/* <img src={LogoIcon} class={`${s().logoIcon} leading-none select-none color-(--foreground)`} alt="mail" /> */}
+      {showName() && (
+        <span
+          style="font-family: 'Lobster', cursive;"
+          class={`${s().name} leading-none select-none`}
         >
-            <img src={LogoIcon} class={`${s().logoIcon} leading-none select-none`} alt="mail" />
-            {showName() && (
-                <span
-                    style="font-family: 'Lobster', cursive;"
-                    class={`${s().name} leading-none select-none`}
-                >
-                    {APP_NAME}
-                </span>
-            )}
-        </div>
-    );
+          {APP_NAME}
+        </span>
+      )}
+    </div>
+  );
 };
 
 export default Logo;
--- src/routes/(public)/auth.tsx
+++ src/routes/(public)/auth.tsx
@@ -4,9 +4,9 @@ import Logo from '~/components/Logo';
 
 export default function AuthLayout(props: RouteSectionProps) {
     return (
-        <div class="min-h-screen flex flex-col gap-10 items-center justify-center">
+        <div class="min-h-screen flex flex-col gap-10 items-center justify-center  bg-(--background)">
             <Logo size="xl" />
-            <div class="w-95 border-2 border-(--border) rounded flex items-center justify-center bg-white/10 p-10 sm:p-8">
+            <div class="w-95 border-2 border-(--border) rounded flex items-center justify-center p-10 sm:p-8 bg-(--blackOrWhite)">
                 {props.children} {/* ✅ replaces <Outlet /> */}
             </div>
         </div>
